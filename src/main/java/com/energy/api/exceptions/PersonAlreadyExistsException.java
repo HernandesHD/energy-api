13e0345019a85a9177487d42b64059fb2081bc9f
HernandesHD <hernandes.andrade@tamboro.com.br>
2023-07-05 20:54:00 -0300

    Revert "Final commit."
    This reverts commit 4536da59d70dfb9098da2bdc9f66a5b66cf639cf.

--- src/main/java/com/energy/api/exceptions/PersonAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.energy.api.exceptions;
-
-public class PersonAlreadyExistsException extends RuntimeException {
-    public PersonAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/energy/api/repository/HomeApplianceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energy.api.repository;
-
-import com.energy.api.entity.HomeAppliances;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HomeApplianceRepository extends JpaRepository<HomeAppliances, Long> {
-}
