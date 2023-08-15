664c34d8d34a2e2dbb42ef7cb314080a840c2999
HernandesHD <hernandes.andrade@tamboro.com.br>
2023-08-15 03:40:34 -0300

    General adjustments.

--- src/main/java/com/energy/api/entity/AddressDTO.java
+++ src/main/java/com/energy/api/entity/AddressDTO.java
@@ -3,7 +3,6 @@ package com.energy.api.entity;
 import jakarta.validation.constraints.*;
 
 public record AddressDTO(
-<<<<<<< HEAD
         @NotEmpty(message = "The { Road } field cannot be empty")
         @Size(max = 60, message = "The { Road } field size must be between 0 and 60")
         @Pattern(regexp = "[a-zA-Z]+", message = "The { Road } field must contain only letters")
@@ -13,19 +12,7 @@ public record AddressDTO(
         Integer number,
         @NotEmpty(message = "The { District } field cannot be empty")
         String district,
-        @NotEmpty(message = "The { City } field cannot be empty")
-=======
-        @NotEmpty(message = "[Address] The { Road } field cannot be empty")
-        @Size(max = 60, message = "[Address] The { Road } field size must be between 0 and 60")
-        @Pattern(regexp = "[a-zA-Z]+", message = "[Address] The { Road } field must contain only letters")
-        String road,
-        @NotNull(message = "[Address] The { Number } field cannot be null")
-        @Positive(message = "[Address] The { Number } field cannot be negative")
-        Integer number,
-        @NotEmpty(message = "[Address] The { District } field cannot be empty")
-        String district,
         @NotEmpty(message = "[Address] The { City } field cannot be empty")
->>>>>>> 8c86a07bf7eb7a0225ca11318b5ae43963298b73
         String city
 ) {
 
--- src/main/java/com/energy/api/entity/PersonRelationDTO.java
+++ src/main/java/com/energy/api/entity/PersonRelationDTO.java
@@ -8,7 +8,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 public record PersonRelationDTO(
-<<<<<<< HEAD
         @Size(min = 3, max = 50, message = "The first name relation field size must be between {min} and {max}.")
         String relationshipType,
 
@@ -23,48 +22,16 @@ public record PersonRelationDTO(
         //@JsonFormat(shape = JsonFormat.Shape.STRING)
         @NotNull(message = "The gender field cannot be empty.")
         Gender prGender,
-
         //@NotNull(message = "[Person] The date of birth  field cannot be empty.")
         @Past(message = "The date of birth in date of birth field must be before the current date.")
         @DateTimeFormat(style = "yyyy-MM-dd")
         LocalDate prDateOfBirth,
-
         @NotBlank(message = "The email field cannot be empty.")
         @Email(message = "The email is invalid.")
         String prEmail,
-
         @NotBlank(message = "The cpf field cannot be empty.")
         @Size(min = 11, max = 11, message = "The cpf field must have 11 characters.")
         @CPF(message = "The CPF is invalid.")
-=======
-        @Size(min = 3, max = 50, message = "[Person Relation] The first name relation field size must be between {min} and {max}.")
-        String relationshipType,
-
-        @NotEmpty(message = "[Person Relation] The first name field cannot be empty.")
-        @Size(min = 2, max = 25, message = "[Person Relation] The first name field size must be between {min} and {max}.")
-        @Pattern(regexp = "[a-zA-ZÀ-ú]+", message = "[Person Relation] The first name field must contain only letters.")
-        String prFirstName,
-        @NotEmpty(message = "[Person Relation] The last name field cannot be empty.")
-        @Size(max = 25, message = "[Person Relation] The last name field size must be between 0 and 25.")
-        @Pattern(regexp = "[a-zA-ZÀ-ú]+", message = "[Person Relation] The last name field must contain only letters.")
-        String prLastName,
-        //@JsonFormat(shape = JsonFormat.Shape.STRING)
-        @NotNull(message = "[Person Relation] The gender field cannot be empty.")
-        Gender prGender,
-
-        //@NotNull(message = "[Person] The date of birth  field cannot be empty.")
-        @Past(message = "[Person Relation] The date of birth in date of birth field must be before the current date.")
-        @DateTimeFormat(style = "yyyy-MM-dd")
-        LocalDate prDateOfBirth,
-
-        @NotBlank(message = "[Person Relation] The email field cannot be empty.")
-        @Email(message = "[Person Relation] The email is invalid.")
-        String prEmail,
-
-        @NotBlank(message = "[Person Relation] The cpf field cannot be empty.")
-        @Size(min = 11, max = 11, message = "[Person Relation] The cpf field must have 11 characters.")
-        @CPF(message = "[Person Relation] The CPF is invalid.")
->>>>>>> 8c86a07bf7eb7a0225ca11318b5ae43963298b73
         String prCpf
 ) {
 }
